8a2766ffef102a04e1fd803ea8ca223923ab91dd
Christopher Wood <christopherwood07@gmail.com>
2015-06-06 12:50:11 -0700

    client code

---
+++ pplclient/app/app.module.js
@@ -0,0 +1,10 @@
+var pplApp = angular.module('pplApp', []);
+
+pplApp.controller('PPLController', function ($scope, $http){
+
+	// TODO
+	
+	// $http.get('countries.json').success(function(data) {
+	// 	$scope.countries = data;
+	// });
+});

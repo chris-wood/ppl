ca9f30cc3711c4cd86d58778a7fc0db68306ef6e
Christopher Wood <christopherwood07@gmail.com>
2015-06-07 14:07:52 -0700

    ngview single page app

--- pplclient/app/app.module.js
+++ pplclient/app/app.module.js
@@ -1,9 +1,30 @@
-var pplApp = angular.module('pplApp', []);
+var pplApp = angular.module('pplApp', ['ngRoute']);
 
-pplApp.controller('PPLController', function ($scope, $http){
+pplApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl : 'app/pages/main.html',
+        controller  : 'mainController'
+    })
+    .when('/papers', {
+        templateUrl : 'app/pages/papers.html',
+        controller  : 'paperController'
+    });
+}]);
 
-	// TODO
-	
+pplApp.controller('mainController', function ($scope, $http){
+	$scope.message = 'Everyone come and see how good I look!';
+});
+
+pplApp.controller('paperController', function($scope, $http) {
+    $scope.message = 'Look! I am an about page.';
+    $scope.papers = 
+		[
+			{name: "test1"},
+			{name: "test2"},
+			{name: "test3"},
+			{name: "test4"},
+		];
+
 	// $http.get('countries.json').success(function(data) {
 	// 	$scope.countries = data;
 	// });

119164e00b79b21b56e29e8a2d0e4b5380d23247
Christopher Wood <christopherwood07@gmail.com>
2015-11-02 18:23:32 -0800

    delete

--- pplserver/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# mongoimport --db ppl --collection authors --file authors.js
-# mongoimport --db ppl --collection venues --file venues.js
-mongoimport --db ppl --jsonArray --collection papers --file papers.js # --jsonArray handles \n characters
--- pplserver/papers.js
+++
@@ -1,5 +0,0 @@
-{
-	title:"Title", 
-	authors:[{name:"Chris Wood", affl: "UCI", email: "woodc1@uci.edu", phone: "3158065939"}], 
-	venue: {name:"ICN", publisher:"ACM", day:1, month:1, year:2015}
-}
